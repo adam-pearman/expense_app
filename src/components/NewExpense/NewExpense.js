8cc530fa65f54e870db867826db69c8a9170a2fe
Adam <104145646+adam-pearman@users.noreply.github.com>
2022-09-22 20:34:59 +0100

    Update NewExpense.js

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,18 +1,31 @@
 import './NewExpense.css';
 import ExpenseForm from "./ExpenseForm";
+import {useState} from "react";
 
 const NewExpense = (props) => {
+    const [showExpenseForm, setShowExpenseForm] = useState(false);
+
     const handleSaveExpenseData = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         props.onAddExpense(expenseData);
+        handleHideExpenseForm();
+    }
+
+    const handleShowExpenseForm = () => {
+        setShowExpenseForm(true);
+    }
+
+    const handleHideExpenseForm = () => {
+        setShowExpenseForm(false);
     }
 
     return (
       <div className="new-expense">
-        <ExpenseForm onSaveExpenseData={handleSaveExpenseData}/>
+          {!showExpenseForm && <button onClick={handleShowExpenseForm}>Add New Expense</button>}
+          {showExpenseForm && <ExpenseForm onSaveExpenseData={handleSaveExpenseData} onHideExpenseForm={handleHideExpenseForm}/>}
       </div>
     );
 }
